2f5eff3d7d506abc11258935df09283686f0491a
KouE10e <210100037+dmk2E@users.noreply.github.com>
2025-09-11 12:45:00 +0900

    jsonファイルのfetchお試し実装

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // 定数定義
 const GITHUB_PAGE = "https://github.com/dmk2E";
+const EXPLANATION_URL = "./explanation.json";
+const EXPLANATION_CONFIG = {};
 
 // 関数定義
 function navigate(url){
     window.location.href = url;
+}
+
+async function getJson(url,config){
+    try{
+        const data = await fetch(url,config);
+        const json = await data.json();
+        return json;
+    }catch(err){
+        throw new Error("Data installation is failured!");
+    }
 }
 
 // メイン処理
@@ -16,3 +28,12 @@ myIcon.addEventListener(
         navigate(GITHUB_PAGE);
     }
 );
+
+getJson(EXPLANATION_URL,EXPLANATION_CONFIG)
+    .then(data =>{
+        console.log(data);
+        // 説明追加の処理
+    })
+    .catch(err =>{
+        console.log(err);
+    });
