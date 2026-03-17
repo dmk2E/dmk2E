50a8bcf1ac9b9ddc3a54317a5b7c330374d7bb27
KouE10e <210100037+dmk2E@users.noreply.github.com>
2026-03-17 13:55:31 +0000

    feat(HOME): Create "RelatedLinks" component #16

--- my-portfolio-site/src/pages/Home/Home.tsx
+++ my-portfolio-site/src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import myIcon from "@/assets/my_icon.svg";
 import "./Home.css";
 import Profile from "@/components/Profile/Profile";
+import RelatedLinks from "@/components/RelatedLinks/RelatedLinks";
 
 export default function Home() {
   return (
     <div id="home_page">
       <Profile iconImage={myIcon}/>
+      <RelatedLinks />
     </div>
   )
 }
