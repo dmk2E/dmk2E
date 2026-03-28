284d75e02eea07f3bef3fca0592cccb01db753e1
KouE10e <210100037+dmk2E@users.noreply.github.com>
2026-03-28 11:45:41 +0000

    feat(AWARDS): Complete the page #22

---
+++ my-portfolio-site/src/components/Achievements/Achievements.tsx
@@ -0,0 +1,59 @@
+import "./Achievements.css";
+import type { DefaultProps } from "@/util";
+import clsx from "clsx";
+// Contentful関係
+import type { Entry } from "contentful";
+import type { AchievementSkeleton } from "@/util";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+
+type AchievementsProps = DefaultProps & {
+  items: Array<Entry<AchievementSkeleton, "WITHOUT_LINK_RESOLUTION", string>>
+};
+
+/**
+ * "emphasis" クラスを付加する関数．
+ * （クリック時に，details タグの summaryタグの文字列を太字にする）
+ * @param evt イベントオブジェクト
+ */
+function handleSummaryToggle(evt: React.SyntheticEvent<HTMLElement>): void{
+  evt.stopPropagation();
+  evt.currentTarget.classList.toggle("emphasis");
+}
+
+export default function Achievements( props: AchievementsProps ){
+  return (
+    <fieldset 
+    className={clsx("achievements", props.className)} 
+    style={props.style} 
+    >
+      <legend>🥇<span className="caption">Achievements</span></legend>
+      {props.items.map(item =>
+        <details 
+        key={item.sys.id} 
+        onToggle={handleSummaryToggle}
+        >
+          <summary>
+            {documentToReactComponents(
+              /* richTextDocument = */ item.fields.heading, 
+              /* options = */ {
+                renderNode: {
+                  [BLOCKS.PARAGRAPH]: (_, children) => <span>{children}</span>, 
+                  [INLINES.HYPERLINK]: (node, children) => <a href={node.data.uri} target="_blank" rel="noopener noreferrer">{children}</a>
+                }
+              }
+            )}
+          </summary>
+          {documentToReactComponents(
+            /* richTextDocument = */ item.fields.content, 
+            /* options = */ {
+              renderNode: {
+                [INLINES.HYPERLINK]: (node, children) => <a href={node.data.uri} target="_blank" rel="noopener noreferrer">{children}</a>
+              }
+            }
+          )}
+        </details>
+      )}
+    </fieldset>
+  );
+}
--- my-portfolio-site/src/pages/Awards/Awards.tsx
+++ my-portfolio-site/src/pages/Awards/Awards.tsx
@@ -1,18 +1,40 @@
 import "./Awards.css";
 import type { DefaultProps } from "@/util";
+import { useState, useEffect } from "react";
+import Achievements from "@/components/Achievements/Achievements";
+// Contentful 関係
+import type { Entry } from "contentful";
+import { client } from "@/util";
+import type { AchievementSkeleton } from "@/util";
 
 type AwardsProps = DefaultProps & {
 
 };
 
 export default function Awards( props: AwardsProps ){
+  // Contentful からデータの抽出
+  const [achievements, setAchievements] = useState<Array<Entry<AchievementSkeleton, "WITHOUT_LINK_RESOLUTION", string>>>(/* initialState = */ []);
+  useEffect(() =>{
+    (async function getContentfulData(){
+      try{
+        const res = await client.getEntries<AchievementSkeleton>({
+          content_type: "achievement", 
+          order: ["-fields.date"]
+        });
+        setAchievements(/* value = */ res.items);
+      }catch(err){
+        console.error("Fetching Contentful data error!");
+      }
+    })();
+  }, []);
+  
   return (
     <div 
     id="awards_page" 
     className={props.className} 
     style={props.style}
     >
-      {/* TODO: Figmaでデザインしてから実装する */}
+      <Achievements items={achievements} />
     </div>
   );
 }
